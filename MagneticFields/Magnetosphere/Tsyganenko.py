f97b89517844a3b97e7835bde089efc848900d21
AntiNeutrino27 <norayrgalikyan@gmail.com>
2024-04-12 23:13:45 +0300

    Finishing Tsyganenko

--- MagneticFields/Magnetosphere/Tsyganenko.py
+++ MagneticFields/Magnetosphere/Tsyganenko.py
@@ -1,8 +1,8 @@
 import datetime
 import numpy as np
-import pyspedas
 
 from MagneticFields import AbsBfield
+from MagneticFields.Magnetosphere.Functions import transformations, t89, t96
 
 
 class Tsyganenko(AbsBfield):
@@ -13,32 +13,57 @@ class Tsyganenko(AbsBfield):
         self.Date = date
         self.ModCode = ModCode
 
-        T96 = np.load("MagneticFields/Magnetosphere/T96input_short.npy", allow_pickle=True).item()
+        T96 = np.load("MagneticFields/Magnetosphere/Data/T96input_short.npy", allow_pickle=True).item()
         self.T96input = T96["T96input"]
         self.T96_date = T96["date"]
 
-        self.Year, self.DoY, self.Secs, self.DTnum = [0]*4
+        self.Year, self.DoY, self.Secs, self.DTnum = 1, 1, 0, 1
 
         if self.Date == 0:
             self.ps = 0
             self.iopt = [1.1200, 2.0000, 1.6000, -0.2000]
+            self.Date = datetime.datetime(1, 1, 1)
         elif isinstance(self.Date, datetime.datetime):
             self.Year = self.Date.year
             self.Doy = self.Date.timetuple().tm_yday
             self.Secs = self.Date.second
-            self.DTnum = self.Date.toordinal() + 366
+            self.DTnum = self.Date.toordinal()  # + 366
 
             self.ps = self.GetPsi()
             self.iopt = self.GetTsyganenkoInd()
 
     def GetPsi(self):
-        pass
+        [x, y, z] = transformations.geo2mag_eccentric(0, 0, 1, 0, datetime.datetime.fromordinal(self.DTnum))
+        [x, y, z] = transformations.gei2geo(x, y, z, self.Year, self.Doy, self.Secs, 0)
+        [x, y, z] = transformations.gei2gsm(x, y, z, self.Year, self.Doy, self.Secs, 1)
+        psi = np.arccos(z / np.linalg.norm([x, y, z]))
+
+        return psi
 
     def GetTsyganenkoInd(self):
-        pass
+
+        ia = np.where((self.T96_date[:, 0] == self.Date.year) * (self.T96_date[:, 1] == self.Date.month) *
+                      (self.T96_date[:, 3] == self.Date.day) * (self.T96_date[:, 4] == self.Date.hour))[0][0]
+
+        ind = None
+        if self.ModCode == 89:
+            ind = self.T96input[ia, -1]
+        elif self.ModCode == 96:
+            ind = self.T96input[ia, :4]
+
+        return ind
 
     def GetBfield(self, x, y, z, **kwargs):
-        pass
+        X, Y, Z = transformations.geo2gsm(x, y, z, self.Year, self.DoY, self.Secs, 1)
+        Bx, By, Bz = 0, 0, 0
+        if self.ModCode == 89:
+            Bx, By, Bz = t89.t89(self.iopt, self.ps, X, Y, Z)
+        elif self.ModCode == 96:
+            Bx, By, Bz = t96.t96(self.iopt, self.ps, X, Y, Z)
+
+        Bx, By, Bz = transformations.geo2gsm(Bx, By, Bz, self.Year, self.DoY, self.Secs, 0)
+
+        return Bx, By, Bz
 
     def UpdateState(self, new_date):
         pass
