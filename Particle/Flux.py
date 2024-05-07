dd8aa4b2130d2f55888d6b86038783b71ead69dc
diag4nd <dyagilev_03@bk.ru>
2024-05-07 17:55:06 +0300

    Added velocity change ability

--- Particle/Flux.py
+++ Particle/Flux.py
@@ -16,13 +16,14 @@ class GeneratorModes(Enum):
 # TODO: add others functions of distribution of energy
 class Flux(Sequence, ABC):
     def __init__(self, Names='pr', Mode: GeneratorModes | str = GeneratorModes.Inward, Radius=1, Center=np.zeros(3),
-                 Nevents: int = 1, ToMeters=1, *args, **kwargs):
+                 Nevents: int = 1, ToMeters=1, V0 = None,  *args, **kwargs):
         self.Mode = Mode if isinstance(Mode, GeneratorModes) else GeneratorModes["Mode"]
         self.Nevents = Nevents
         self.Center = Center
         self.Radius = Radius
         self.ToMeters = ToMeters
         self.Names = Names
+        self.V0 = V0
         self.args = args
         self.kwargs = kwargs
         self.particles = []
@@ -63,19 +64,28 @@ class Flux(Sequence, ABC):
 
                 S = np.stack((newX.T, newY.T, newZ.T), axis=1)
 
+
                 ksi = np.random.rand(1, 1, self.Nevents)
                 sin_theta = np.sqrt(ksi)
                 cos_theta = np.sqrt(1 - ksi)
                 phi = 2 * np.pi * np.random.rand(1, 1, self.Nevents)
                 p = np.concatenate((-sin_theta * np.cos(phi), -sin_theta * np.sin(phi), -cos_theta))
                 self.r = r * Ro + Rc
-                self.v = np.concatenate([S[:, :, i] @ p[:, :, i] for i in range(self.Nevents)], axis=1).T
+                if self.V0 is None:
+                    self.v = np.concatenate([S[:, :, i] @ p[:, :, i] for i in range(self.Nevents)], axis=1).T
+                else:
+                    self.v = self.V0
+
 
             case GeneratorModes.Outward:
                 self.r = np.tile(Rc, (self.Nevents, 1))
                 theta = np.arccos(1 - 2 * np.random.rand(self.Nevents, 1))
                 phi = 2 * np.pi * np.random.rand(self.Nevents, 1)
-                self.v = np.array([np.sin(theta) * np.cos(phi), np.sin(theta) * np.sin(phi), np.cos(theta)])
+                if self.V0 is None:
+                    self.v = np.array([np.sin(theta) * np.cos(phi), np.sin(theta) * np.sin(phi), np.cos(theta)])
+                else:
+                    self.v = self.V0
+
 
     def __getitem__(self, item):
         return self.particles[item]
