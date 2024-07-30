69fec557ce3db53e119766ab6826cc532bd31a13
rustam <starwarskust@gmail.com>
2024-07-30 16:11:14 +0300

    update GTnrlmsis

--- Medium/Magnetosphere/GTnrlmsis.py
+++ Medium/Magnetosphere/GTnrlmsis.py
@@ -9,11 +9,12 @@ from Medium import GTGeneralMedium
 
 class GTnrlmsis(GTGeneralMedium):
 
-    def __init__(self, date: datetime.datetime):
+    def __init__(self, date: datetime.datetime, version=2.1):
         super().__init__()
         self.region = Regions.Magnetosphere
         self.model = "NRLMSIS"
-        # self.model_elements = ['He', 'O', 'N2', 'O2', 'Ar', 'H', 'N']
+        self.version = version
+        self.element_list = ['N2', 'O2', 'O', 'He', 'H', 'Ar', 'N', 'O_anomalous', 'NO']
         self.date = date
         f107, f107a, ap = msis.get_f107_ap(self.date)
         self.f107 = f107
@@ -25,19 +26,16 @@ class GTnrlmsis(GTGeneralMedium):
     def calculate_model(self, x, y, z, **kwargs):
         lon, lat, alt = self.convert_xyz_to_lla(x, y, z)
         alt *= 1e-3 # m -> km
-        model_output = msis.run(self.date, lon, lat, alt, self.f107, self.f107a, [self.ap])[0]
-        self.density = model_output[0] # kg/m3
-        # self.element_abundance = model_output[[0, 1, 2, 3, 4, 6, 7]] / np.sum(model_output[[0, 1, 2, 3, 4, 6, 7]])
+        self.model_output = msis.run(self.date, lon, lat, alt, self.f107, self.f107a, [self.ap], version=self.version)[0]
 
     def convert_xyz_to_lla(self, x, y, z):
         return self.transformer.transform(x, y, z, radians=False)
 
     def get_density(self):
-        return self.density # g/сm3
+        return self.model_output[0] # kg/m3
 
     def get_element_abundance(self):
-        pass
-        # return self.element_abundance
+        return self.model_output[1:-1] / np.nansum(model_output[1:-1])
 
     def __str__(self):
-        return f'NRLMSIS'
+        return f'NRLMSIS-{self.version}'
