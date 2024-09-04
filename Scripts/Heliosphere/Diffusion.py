248dbaf80073f21740befb650f19b6be07ee6d83
AntiNeutrino27 <norayrgalikyan@gmail.com>
2024-09-04 14:02:33 +0300

    Updated diffusion

--- Scripts/Heliosphere/Diffusion.py
+++ Scripts/Heliosphere/Diffusion.py
@@ -21,7 +21,7 @@ c_cm = 3e10
 c_au = c_cm/au2cm
 
 dt = 0.01
-Ns = np.logspace(1, 5, 30, dtype=int)
+Ns = np.logspace(1, 5.8, 30, dtype=int)
 
 def update_lines(num, walks, lines):
     for line, walk in zip(lines, walks):
@@ -42,18 +42,13 @@ def basis(Bx, By, Bz):
 
     return ex, ey, ez
 
-paths = ["../../tests/DDiffusionPRReg0"]
+paths = ["../../tests/DiffNew1"]
 Bx, By, Bz = None, None, None
 ex, ey, ez = None, None, None
+time = None
 
 for path in paths:
     files = os.listdir(path)
-    Kzz_arr = []
-    Kxx_arr = []
-    Kyy_arr = []
-    Kxy_arr = []
-    Kyx_arr = []
-
 
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
@@ -73,6 +68,7 @@ for path in paths:
                 Bx, By, Bz = parker.CalcBfield(X[0], Y[0], Z[0])
                 B = np.sqrt(Bx**2 + By**2 + Bz**2)
                 ex, ey, ez = basis(Bx, By, Bz)
+                time = np.arange(len(X))[1:]*dt
             walks.append(R[:Ns[-1]:50])
             # ax.set_xlabel(f"X [AU]")
             # ax.set_ylabel(f"Y [AU]")
@@ -103,86 +99,83 @@ for path in paths:
     plt.legend()
     plt.show()
 
+    Kzz = []
+    Kxx = []
+    Kyy = []
+    Kxy = []
+    Kyx = []
 
-    for N in tqdm(Ns):
-        tau = dt * N
-        Kzz = []
-        Kxx = []
-        Kyy = []
-        Kxy = []
-        Kyx = []
 
-        for file in files:
-            if not file.endswith(".npy"):
-                continue
-            particles = np.load(path + os.sep + file, allow_pickle=True)
-            for event in particles:
-                T = event["Particle"]["T0"]
-                M = event["Particle"]["M"]
-                V_norm = np.sqrt(1 - (M/(T+M))**2) * c_au
+    for file in files:
+        if not file.endswith(".npy"):
+            continue
+        particles = np.load(path + os.sep + file, allow_pickle=True)
+        for event in particles:
+            T = event["Particle"]["T0"]
+            M = event["Particle"]["M"]
+            V_norm = np.sqrt(1 - (M/(T+M))**2) * c_au
 
-                R = event["Track"]["Coordinates"]
-                X, Y, Z = R[:, 0], R[:, 1], R[:, 2]
+            R = event["Track"]["Coordinates"]
+            X, Y, Z = R[:, 0], R[:, 1], R[:, 2]
 
-                V = event["Track"]["Velocities"][0] * V_norm
+            V = event["Track"]["Velocities"][0] * V_norm
 
-                if Bx is None:
-                    Bx, By, Bz = parker.CalcBfield(X[0], Y[0], Z[0])
-                    ex, ey, ez = basis(Bx, By, Bz)
+            if Bx is None:
+                Bx, By, Bz = parker.CalcBfield(X[0], Y[0], Z[0])
+                ex, ey, ez = basis(Bx, By, Bz)
+                time = np.arange(len(X))[1:]*dt
 
-                delta_r = np.array([X[N] - X[0],
-                                    Y[N] - Y[0],
-                                    Z[N] - Z[0]])
+            delta_r = R[1:] - R[0, :]
 
-                delta_r_z = delta_r @ ez
-                delta_r_x = delta_r @ ex
-                delta_r_y = delta_r @ ey
+            delta_r_z = delta_r @ ez
+            delta_r_x = delta_r @ ex
+            delta_r_y = delta_r @ ey
 
-                Vx = V @ ex
-                Vy = V @ ey
+            Vx = V @ ex
+            Vy = V @ ey
 
-                Kzz.append(delta_r_z**2 / 2*tau)
-                Kxx.append(delta_r_x**2 / 2*tau)
-                Kyy.append(delta_r_y**2 / 2*tau)
+            Kzz.append(delta_r_z**2 / (2*time))
+            Kxx.append(delta_r_x**2 / (2*time))
+            Kyy.append(delta_r_y**2 / (2*time))
 
-                Kxy.append(delta_r_x*Vy)
-                Kyx.append(delta_r_y*Vx)
+            Kxy.append(delta_r_x*Vy)
+            Kyx.append(delta_r_y*Vx)
 
-        Kzz_arr.append(np.mean(Kzz))
-        Kxx_arr.append(np.mean(Kxx))
-        Kyy_arr.append(np.mean(Kyy))
+    Kzz = np.array(Kzz)
+    Kxx = np.array(Kxx)
+    Kyy = np.array(Kyy)
 
-        Kxy_arr.append(np.mean(Kxy))
-        Kyx_arr.append(np.mean(Kyx))
+    Kxy = np.array(Kxy)
+    Kyx = np.array(Kyx)
 
-plt.figure()
-plt.plot(Ns*dt, np.array(Kzz_arr)*au2cm**2)
-plt.legend()
-plt.ylabel("K, [$cm^2/sec$]")
-plt.xlabel("t, [sec]")
-plt.xscale('log')
-plt.yscale('log')
-plt.title("$K_\parallel $")
+    plt.figure()
+    plt.plot(time, np.mean(Kzz, axis=0)*au2cm**2)
+    plt.legend()
+    plt.ylabel("K, [$cm^2/sec$]")
+    plt.xlabel("t, [sec]")
+    plt.xscale('log')
+    plt.yscale('log')
+    plt.title("$K_\parallel $")
 
-plt.figure()
-plt.plot(Ns*dt, np.array(Kxx_arr)*au2cm**2, label="Kxx")
-plt.plot(Ns*dt, np.array(Kyy_arr)*au2cm**2, label="Kyy")
-plt.legend()
-plt.ylabel("K, [$cm^2/sec$]")
-plt.xlabel("t, [sec]")
-plt.xscale('log')
-plt.yscale('log')
-plt.title("$K_\perp$")
+    plt.figure()
+    plt.plot(time, np.mean(Kxx, axis=0)*au2cm**2, label="Kxx")
+    plt.plot(time, np.mean(Kyy, axis=0)*au2cm**2, label="Kyy")
+    plt.legend()
+    plt.ylabel("K, [$cm^2/sec$]")
+    plt.xlabel("t, [sec]")
+    plt.xscale('log')
+    plt.yscale('log')
+    plt.title("$K_\perp$")
 
-plt.figure()
-plt.plot(Ns*dt, np.array(Kxy_arr)*au2cm**2, label="Kxy")
-plt.ylabel("K, [$cm^2/sec$]")
-plt.xlabel("t, [sec]")
-plt.ylabel("K, [$cm^2/sec$]")
-plt.plot(Ns*dt, np.array(Kyx_arr)*au2cm**2, label="Kyx")
-plt.legend()
-plt.xscale('log')
-plt.title("$K_A$")
+    plt.figure()
+    plt.plot(time, np.mean(Kxy, axis=0)*au2cm**2, label="Kxy")
+    plt.ylabel("K, [$cm^2/sec$]")
+    plt.xlabel("t, [sec]")
+    plt.ylabel("K, [$cm^2/sec$]")
+    plt.plot(time, np.mean(Kyx, axis=0)*au2cm**2, label="Kyx")
+    plt.legend()
+    plt.xscale('log')
+    plt.title("$K_A$")
 
-plt.show()
+    plt.show()
 
