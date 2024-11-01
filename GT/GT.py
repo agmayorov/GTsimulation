2a638eb255740066601c774afa081feebb5659f9
rustam <starwarskust@gmail.com>
2024-11-01 17:00:01 +0300

    fix lepton exclusion

--- GT/GT.py
+++ GT/GT.py
@@ -426,7 +426,7 @@ class GTSimulator(ABC):
             raise ValueError('Nuclear Interaction is enabled but Medium is not set')
         self.InteractNUC = UseInteractNUC
         if self.InteractNUC is not None and 'l' in self.InteractNUC.get("ExcludeParcticleList", []):
-            self.InteractNUC['ExcludeParcticleList'].append([ 11,  12,  13,  14,  15,  16,  17,  18,
+            self.InteractNUC['ExcludeParcticleList'].extend([ 11,  12,  13,  14,  15,  16,  17,  18,
                                                              -11, -12, -13, -14, -15, -16, -17, -18])
         self.IntPathDen = 10 # g/cm2
         if self.Verbose:
