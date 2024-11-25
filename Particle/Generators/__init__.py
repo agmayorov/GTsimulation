d2715b1c27c2a5ea1e0629bcb8e3d9417ffa8bd1
AntiNeutrino27 <norayrgalikyan@gmail.com>
2024-11-25 13:09:43 +0300

    The new spatial particle generator added to __init__.py

--- Particle/Generators/__init__.py
+++ Particle/Generators/__init__.py
@@ -1,2 +1,2 @@
 from .Spectrums import Monolines, PowerSpectrum, Uniform
-from .Distributions import SphereSurf, Disk, GeneratorModes
+from .Distributions import SphereSurf, Disk, GeneratorModes, SphereVol
