bugfix: proper units in GetGCRflux input, code style

Particle/Generators/Spectrums.py
@@ -4,7 +4,7 @@ import numpy as np
 
 from Particle.GeneratorCR import GetGCRflux
 from Particle.GetNucleiProp import GetNucleiProp
-from Particle.functions import ConvertUnits
+from Particle.functions import convert_units
 
 
 class AbsSpectrum(ABC):
@@ -65,7 +65,7 @@ class PowerSpectrum(ContinuumSpectrum):
         for s in range(self.flux.Nevents):
             a, z, m, *_ = GetNucleiProp(self.flux.ParticleNames[s])
             if self.energy_range_units != self.base:
-                energy_range_s = ConvertUnits(self.energy_range, self.energy_range_units, self.base, m, a, z)
+                energy_range_s = convert_units(self.energy_range, self.energy_range_units, self.base, m, a, z)
             else:
                 energy_range_s = self.energy_range
 
@@ -77,7 +77,7 @@ class PowerSpectrum(ContinuumSpectrum):
                 energy[s] = (energy_range_s[0] ** g + ksi * (energy_range_s[1] ** g - energy_range_s[0] ** g)) ** (1 / g)
 
             if self.energy_range_units != self.base:
-                energy[s] = ConvertUnits(energy[s], self.base, self.energy_range_units, m, a, z)
+                energy[s] = convert_units(energy[s], self.base, self.energy_range_units, m, a, z)
         return energy
 
     def __str__(self):
@@ -100,17 +100,17 @@ class ForceField(ContinuumSpectrum):
         for i, particle in enumerate(unique_particle):
             a, z, m, *_ = GetNucleiProp(particle)
             if self.energy_range_units != 'T':
-                energy_range_s = ConvertUnits(self.energy_range, self.energy_range_units, 'T', m, a, z)
+                energy_range_s = convert_units(self.energy_range, self.energy_range_units, 'T', m, a, z)
             else:
                 energy_range_s = self.energy_range
 
-            f_max = GetGCRflux('T', np.geomspace(energy_range_s[0], energy_range_s[1], 1000), self.modulation_potential, particle).max()
+            f_max = GetGCRflux('T', np.geomspace(energy_range_s[0], energy_range_s[1], 1000) / 1e3, self.modulation_potential / 1e3, particle).max()
             index_particle = np.flatnonzero(index_inverse == i)
             bunch_size = count[i] * 10 # multiplying by 10 for faster calculations
             while True:
                 energy_played = np.random.uniform(*energy_range_s, bunch_size)
                 ksi = f_max * np.random.rand(bunch_size)
-                index_suited = np.flatnonzero(ksi < GetGCRflux('T', energy_played, self.modulation_potential, particle))
+                index_suited = np.flatnonzero(ksi < GetGCRflux('T', energy_played / 1e3, self.modulation_potential / 1e3, particle))
                 if index_suited.size < index_particle.size:
                     energy[index_particle[:index_suited.size]] = energy_played[index_suited]
                     index_particle = np.delete(index_particle, range(index_suited.size))
@@ -119,7 +119,7 @@ class ForceField(ContinuumSpectrum):
                     break
 
             if self.energy_range_units != 'T':
-                energy[index_inverse == i] = ConvertUnits(energy[index_inverse == i], 'T', self.energy_range_units, m, a, z)
+                energy[index_inverse == i] = convert_units(energy[index_inverse == i], 'T', self.energy_range_units, m, a, z)
         return energy
 
     def __str__(self):

Particle/functions.py
@@ -6,7 +6,7 @@ def ConvertR2T(R, M, A, Z):
     # INPUT:
     #       R - rigidity in GV
     #       M - Mass of particle in GV/c^2
-    #       A - Number of nuclons
+    #       A - Number of nucleons
     #       Z - Charge
     # OUTPUT:
     #       E - Kinetic energy in GeV
@@ -24,7 +24,7 @@ def ConvertT2R(T, M, A, Z):
     # INPUT:
     #       T - Kinetic energy in GeV
     #       M - Mass of particle in GV/c^2
-    #       A - Number of nuclons
+    #       A - Number of nucleons
     #       Z - Charge
     # OUTPUT:
     #       R - Rigidity in GV
@@ -35,28 +35,30 @@ def ConvertT2R(T, M, A, Z):
 
     return R
 
-def ConvertUnits(Energy, FromUnits, ToUnits, M, A, Z):
-    match FromUnits:
-        case 'T':
-            match ToUnits:
-                case 'R':
-                    EnergyConverted = ConvertT2R(Energy, M, A, Z)
-                case 'E':
-                    EnergyConverted = Energy * A
-        case 'R':
-            match ToUnits:
-                case 'T':
-                    EnergyConverted = ConvertR2T(Energy, M, A, Z)
-                case 'E':
-                    EnergyConverted = ConvertR2T(Energy, M, A, Z) * A
-        case 'E':
-            match ToUnits:
-                case 'T':
-                    EnergyConverted = Energy / A
-                case 'R':
-                    EnergyConverted = ConvertT2R(Energy / A, M, A, Z)
 
-    return EnergyConverted
+def convert_units(energy, from_units, to_units, m, a, z):
+    energy_converted = np.zeros_like(energy)
+    match from_units:
+        case 'T':
+            match to_units:
+                case 'R':
+                    energy_converted = ConvertT2R(energy, m, a, z)
+                case 'E':
+                    energy_converted = energy * a
+        case 'R':
+            match to_units:
+                case 'T':
+                    energy_converted = ConvertR2T(energy, m, a, z)
+                case 'E':
+                    energy_converted = ConvertR2T(energy, m, a, z) * a
+        case 'E':
+            match to_units:
+                case 'T':
+                    energy_converted = energy / a
+                case 'R':
+                    energy_converted = ConvertT2R(energy / a, m, a, z)
+
+    return energy_converted
 
 
 def GetAntiParticle(particle):