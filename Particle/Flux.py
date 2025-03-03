e817661ed4013157548d180a0913691a753863c8
AntiNeutrino27 <norayrgalikyan@gmail.com>
2025-03-03 21:18:45 +0300

    Bug with spectra and initial distribution default value fixed

--- Particle/Flux.py
+++ Particle/Flux.py
@@ -3,12 +3,12 @@ from collections.abc import Sequence, Iterable
 import numpy as np
 
 from Particle.Generators import GeneratorModes
-from Particle.Generators import Monolines, SphereSurf
+from Particle.Generators import AbsDistribution, AbsSpectrum
 from Particle.Particle import CRParticle, Particle
 
 
 class Flux(Sequence):
-    def __init__(self, Spectrum=Monolines, Distribution=SphereSurf, Names='pr', Nevents: int = 1, ToMeters=1, V0=None,
+    def __init__(self, Spectrum: AbsSpectrum, Distribution: AbsDistribution, Names='pr', Nevents: int = 1, ToMeters=1, V0=None,
                  Mode: GeneratorModes | str = GeneratorModes.Inward, *args, **kwargs):
         self.Mode = Mode if isinstance(Mode, GeneratorModes) else GeneratorModes[Mode]
         self.Nevents = Nevents
