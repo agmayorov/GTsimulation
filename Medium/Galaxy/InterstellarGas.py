9c14f29b37c719b827fe7ebfb68fce690266d447
rustam <starwarskust@gmail.com>
2025-03-06 18:05:37 +0300

    bugfix: correct InterstellarGas parametrization

--- Medium/Galaxy/InterstellarGas.py
+++ Medium/Galaxy/InterstellarGas.py
@@ -18,7 +18,7 @@ class InterstellarGas(GTGeneralMedium):
         self.n_HI, self.n_H2 = self.__calculate_b_field(x / Units.kpc, y / Units.kpc, z / Units.kpc)
 
     def get_density(self):
-        return self.n_HI * 1e6 * 1.67e-27 + self.n_H2 * 1e6 * 3.34 * 1e-27
+        return self.n_HI * 1e6 * 1.67e-27 + self.n_H2 * 1e6 * 2 * 1.67e-27
 
     def get_element_abundance(self):
         pass
@@ -44,9 +44,9 @@ class InterstellarGas(GTGeneralMedium):
         z_s_CO = 0.103
         r_z0 = 8.5
         r_z = 6.94
-        w_0p = np.array([2.063e-4, -0.002329, 0.001374, -0.03613])
-        w_1p = np.array([-6.838e-5, -0.005807, 0.063540, -0.11340])
-        w_2p = np.array([1.217e-4, 0.001202, -0.058400, 0.26010])
+        w_0p = np.array([-4.759003e-2, 3.499272e-3, -2.426709e-3, 2.075231e-4])
+        w_1p = np.array([1.412557e-1, -7.066979e-2, 6.260557e-3, 6.192461e-5])
+        w_2p = np.array([2.830553e-1, -6.618279e-2, 1.885424e-3, 1.057025e-4])
         theta_1 = 4.61
         theta_2 = 2.73
 
@@ -55,7 +55,7 @@ class InterstellarGas(GTGeneralMedium):
         theta_b = 5.67
         x_0 = 0.751
         r_b = 0.514
-        z_b = 6.43
+        z_b = 6.43e-3 # kpc
         e_i = 0.647
         p_i = 1.18
 
@@ -67,7 +67,7 @@ class InterstellarGas(GTGeneralMedium):
         n8_s_HI = np.array([0.184, 0.193, 0.332, 0.521])
         n8_s_CO = np.array([0.642, 0, 3.37, 7.53])
 
-        r_max = 20
+        r_max = 25
         theta_max = a * np.log(r_max / r_min) + theta_min
 
         def spiral(phi, r_min, phi_min, alpha):
@@ -88,13 +88,13 @@ class InterstellarGas(GTGeneralMedium):
 
         # --- Warped disk ---
         # Radial profile
-        f_d_HI = 0.2291 * r ** 2 * np.exp(-r / 3.016) / 0.250 # normalized on n8
+        f_d_HI = 0.075 * r ** 3 * np.exp(-r / 2.235) # normalized on n8
         f_d_CO = np.exp(-(r - r_s) / r_0) * (1 - np.exp(-(r / r_h) ** h_i))
 
         # Warp of disk
-        w_0 = polyval(r, w_0p[::-1])
-        w_1 = polyval(r, w_1p[::-1])
-        w_2 = polyval(r, w_2p[::-1])
+        w_0 = polyval(r, w_0p)
+        w_1 = polyval(r, w_1p)
+        w_2 = polyval(r, w_2p)
         z_0 = w_0 + w_1 * np.sin(theta - theta_1) + w_2 * np.sin(2 * theta - theta_2)
 
         # Vertical profile
@@ -112,14 +112,13 @@ class InterstellarGas(GTGeneralMedium):
         xx = x * np.cos(theta_b) + y * np.sin(theta_b) + x_0
         yy = -x * np.sin(theta_b) + y * np.cos(theta_b)
         rr = np.sqrt(xx ** 2 + (yy / 0.3) ** 2)
-        Rr = 1 / (rr / r_b + z / z_b)
+        Rr = 1 / (rr / r_b + np.abs(z) / z_b)
         n_bulge = n_b * np.exp(-Rr ** e_i) * Rr ** p_i
 
         # --- Spiral arms ---
         r_distance = np.zeros(4)
         for i in range(4):
             r_distance[i] = get_distance(theta, r, r_min[i], r_max, theta_min[i], theta_max[i], a[i])
-
         n_s_HI = np.nansum(n8_s_HI * f_d_HI * f_s_HI * np.exp(-r_distance ** 2 / (2 * sigma_arms ** 2)))
         n_s_CO = np.nansum(n8_s_CO * f_d_CO * f_s_CO * np.exp(-r_distance ** 2 / (2 * sigma_arms ** 2)))
 
