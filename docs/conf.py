19e145582d1e8a5c7c467e523d27ab90ac183ed2
rustam <starwarskust@gmail.com>
2025-11-01 15:24:00 +0300

    add icon for site tab

--- docs/conf.py
+++ docs/conf.py
@@ -37,6 +37,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+html_favicon = '_static/favicon.svg'
 
 autodoc_default_options = {
     'members': True,
