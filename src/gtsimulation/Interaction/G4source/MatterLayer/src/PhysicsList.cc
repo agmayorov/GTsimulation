2afd78969056ef7da28639b3520871446ef134cc
rustam <starwarskust@gmail.com>
2026-04-29 16:41:58 +0300

    replace deactivation of nKiller process with its removal

--- src/gtsimulation/Interaction/G4source/MatterLayer/src/PhysicsList.cc
+++ src/gtsimulation/Interaction/G4source/MatterLayer/src/PhysicsList.cc
@@ -13,17 +13,17 @@ PhysicsList::~PhysicsList()
 {}
 
 void PhysicsList::ConstructProcess() {
-    FTFP_BERT::ConstructProcess();
-    // Disable neutron killing process
-    G4ProcessManager* pManager = G4Neutron::Neutron()->GetProcessManager();
-    G4ProcessVector* pVector = pManager->GetProcessList();
-    for (G4int i = 0; i < pVector->size(); ++i) {
-        G4VProcess* process = (*pVector)[i];
-        if (process->GetProcessName() == "nKiller") {
-            pManager->SetProcessActivation(process, false);
-            break;
-        }
+  FTFP_BERT::ConstructProcess();
+  // Remove neutron killing process
+  G4ProcessManager* pManager = G4Neutron::Neutron()->GetProcessManager();
+  G4ProcessVector* pVector = pManager->GetProcessList();
+  for (G4int i = 0; i < pVector->size(); ++i) {
+    G4VProcess* process = (*pVector)[i];
+    if (process->GetProcessName() == "nKiller") {
+      pManager->RemoveProcess(process);
+      break;
     }
+  }
 }
 
 }
