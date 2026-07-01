ef9cb36052a384d3d4750bf61d0d0ad15493c9a5
rustam <starwarskust@gmail.com>
2026-07-01 11:24:54 +0300

    refactor Galaxy subpackages inside MagneticFields subpackage in lowercase mode

--- src/gtsimulation/MagneticFields/__init__.py
+++ src/gtsimulation/MagneticFields/__init__.py
@@ -2,7 +2,7 @@ from ._magnetic_field import AbsBfield
 from ._summed_field import Summed
 from ._uniform import Uniform
 
-from . import magnetosphere, heliosphere, Galaxy
+from . import magnetosphere, heliosphere, galaxy
 
 __all__ = [
     "AbsBfield",
