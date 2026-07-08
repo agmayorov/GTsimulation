33352ed45a7424a837c0e2cab23b289636cd3a7b
rustam <starwarskust@gmail.com>
2026-07-08 12:36:04 +0300

    fix: add RK_fast pushers into the pusher subpackage

--- src/gtsimulation/pusher/__init__.py
+++ src/gtsimulation/pusher/__init__.py
@@ -1,12 +1,14 @@
 from ._buneman_boris import BunemanBorisSimulator
-from ._runge_kutta import RungeKutta4Simulator, RungeKutta6Simulator
+from ._runge_kutta import RungeKutta4Simulator, RungeKutta6Simulator, RungeKutta4SimulatorFast, RungeKutta6SimulatorFast
 from ._higuera_cary import HigueraCarySimulator
 from ._vay import VaySimulator
 
 __all__ = [
     "BunemanBorisSimulator",
     "RungeKutta4Simulator",
+    "RungeKutta4SimulatorFast",
     "RungeKutta6Simulator",
+    "RungeKutta6SimulatorFast",
     "HigueraCarySimulator",
     "VaySimulator",
 ]
