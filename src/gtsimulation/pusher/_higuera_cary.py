e57430a83c3fd1e61decde2cff660139948e8a2a
diag4nd <dyagilev_03@bk.ru>
2026-09-10 10:27:33 +0300

    Buneman-Boris/Vay/Higuera-Cary pushers fixed: half position step updates added.

--- src/gtsimulation/pusher/_higuera_cary.py
+++ src/gtsimulation/pusher/_higuera_cary.py
@@ -6,17 +6,43 @@ from gtsimulation.common import Constants, Units
 
 class HigueraCarySimulator(GTSimulator):
     def AlgoStep(self, T, M, Q, V, X, H, E):
+        dt = self.Step
+
+        # Half position step:
+        # X^(n+1/2) = X^n + v^n * dt / 2
+        X_half = X + V * dt / 2
+        E_half, H_half = self.getFields(X_half)
+
         if M != 0:
-            q = self.Step * Q / 2 / (M * Units.MeV2kg)
-            c = Constants.c
-            Vp, Yp, Ya = self.__algo(E, H, M, T, V, q, c)
+            q = dt * Q / (2 * M * Units.MeV2kg)
+
+            # Velocity update
+            Vp, Yp, Ya = self.__algo(
+                E_half, H_half, M, T, V, q, Constants.c
+            )
         else:
             Vp, Yp, Ya = V, 0, 0
 
-        X_new = X + Vp * self.Step
+        # Second half position step:
+        # X^(n+1) = X^(n+1/2) + v^(n+1) * dt / 2
+        X_new = X_half + Vp * dt / 2
 
         return X_new, Vp, Yp, Ya
 
+    def getFields(self, X):
+        x, y, z = X
+        if self.Bfield is not None:
+            H = np.array(self.Bfield.GetBfield(x, y, z))
+        else:
+            H = np.zeros(3)
+
+        if self.Efield is not None:
+            E = np.array(self.Efield.GetEfield(x, y, z))
+        else:
+            E = np.zeros(3)
+
+        return E, H
+    
     @staticmethod
     @jit(fastmath=True, nopython=True)
     def __algo(E, H, M, T_particle, V, q, c):
